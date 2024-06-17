e5c5c565a554d16cf0ad84689da90f45de6daeba
Developer Williams <archywilliams2@gmail.com>
2024-06-17 19:41:37 +0300

    Upgraded Login Functionality

--- frontend/src/login/Login.jsx
+++ frontend/src/login/Login.jsx
@@ -3,28 +3,29 @@ import "../App.css";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer, toast, Bounce } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
       const response = await axios.post("http://localhost:5000/auth/login", {
         email,
         password,
       });
 
-      // This part will be executed only if the status code is in the range of 2xx
       if (response.data.message === "Login Successful") {
         navigate("/home");
+        toast.success("Login Succesfull")
       }
     } catch (error) {
-      // This part will be executed for any status code outside the range of 2xx
       if (error.response) {
         const { message, redirect } = error.response.data;
 
@@ -35,11 +36,13 @@ const Login = () => {
         } else if (message === "Email Not Verified") {
           navigate(redirect);
         } else {
-          toast.error(message); // Generic toast for any other messages
+          toast.error(message);
         }
       } else {
-        toast.error(error.message); // Fallback toast if there is no response from server
+        toast.error(error.message);
       }
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -47,18 +50,19 @@ const Login = () => {
     <div className="home">
       <ToastContainer
         position="top-center"
-        autoClose={false}
+        autoClose={5000}
         newestOnTop={false}
         closeOnClick
         rtl={false}
         pauseOnFocusLoss
         draggable
+        pauseOnHover
         theme="colored"
-        transition:Bounce
+        transition={Bounce}
       />
       <div className="container">
         <form>
-          <img src="./login-pana.svg" alt="Login" srcSet="" />
+          <img src="./login-pana.svg" alt="Login" />
         </form>
         <form className="myform" onSubmit={handleSubmit}>
           <h3>
@@ -85,7 +89,9 @@ const Login = () => {
               <Link to="/reset-password">Reset Password</Link>
             </div>
             <div className="second-div">
-              <button type="submit">Login</button>
+              <button type="submit" disabled={loading}>
+                {loading ? "Loading..." : "Submit"}
+              </button>
               <p>
                 Not Having Account? <Link to="/signup">Signup</Link>
               </p>
