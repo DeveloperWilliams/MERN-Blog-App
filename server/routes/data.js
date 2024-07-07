3c884eaab8921c11deeafcdc9d5575f69b11601a
Developer William Achuchi <116683476+DeveloperWilliams@users.noreply.github.com>
2024-07-08 01:24:12 +0300

    Routes Upgrade

--- server/routes/data.js
+++ server/routes/data.js
@@ -1,5 +1,5 @@
-import express, { Router } from "express";
-import BlogModel from "../model/Blog.js"
+import express from "express";
+import BlogModel from "../model/Blog.js";
 
 const router = express.Router();
 
@@ -10,7 +10,40 @@ router.post("/create-blog/", async (req, res) => {
     await blog.save();
     res.status(200).json({ message: "Success" });
   } catch (error) {
-    res.status(500).json({ message: error });
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post("/delete-blog/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const blog = await BlogModel.findByIdAndDelete(id);
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+    res.status(200).json({ message: "Blog Deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post("/update-blog/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, image, content } = req.body;
+  try {
+    const blog = await BlogModel.findById(id);
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+
+    blog.title = title || blog.title;
+    blog.image = image || blog.image;
+    blog.content = content || blog.content;
+
+    await blog.save();
+    res.status(200).json({ message: "Blog Updated" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
