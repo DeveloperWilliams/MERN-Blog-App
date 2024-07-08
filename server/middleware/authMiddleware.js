5532505bd26692588790c5211aea288b7e3c4e14
Developer William Achuchi <116683476+DeveloperWilliams@users.noreply.github.com>
2024-07-09 00:35:10 +0300

    Protected route upgrde

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const verifyToken = (req, res, next) => {
-  const token = req.cookie.token;
+  const token = req.cookies.token; 
 
   if (!token) {
     return res.status(401).json({ message: "Access Denied" });
